d0b9c24dff267b88324800e4978d739ce64bd372
Mariana Gomes <marianabicla@gmail.com>
2022-03-22 00:51:27 -0300

    criando novas secoes e ajustando layouts

--- main.js
+++ main.js
@@ -5,28 +5,28 @@ const db = [
     id: 1,
     nome: "Baggio Café Espresso Grão 500g",
     descricao: "Um café premium de alta qualidade com notas intensas",
-    preco: 29.7,
+    preco: "29.70",
     imagem: "./img/img-cafe.svg",
   },
   {
     id: 2,
     nome: "Baggio Café Espresso Grão 500g",
     descricao: "Um café premium de alta qualidade com notas intensas",
-    preco: 29.7,
+    preco: "29.70",
     imagem: "./img/img-cafe.svg",
   },
   {
     id: 3,
     nome: "Baggio Café Espresso Grão 500g",
     descricao: "Um café premium de alta qualidade com notas intensas",
-    preco: 29.7,
+    preco: "29.70",
     imagem: "./img/img-cafe.svg",
   },
   {
     id: 4,
     nome: "Baggio Café Espresso Grão 500g",
     descricao: "Um café premium de alta qualidade com notas intensas",
-    preco: 29.7,
+    preco: "29.70",
     imagem: "./img/img-cafe.svg",
   },
 ];
@@ -38,17 +38,16 @@ const criarCard = (produto) => {
     <div class="card-image-container">
                     <img src="${produto.imagem}" alt="foto do produto" class="card-image">
                 </div>
-                <span class="card-produto">
+                <span class="card-produto nome">
                     ${produto.nome}
                 </span>
-                <span class="card-produto">
+                <span class="card-produto descricao">
                     ${produto.descricao}
                 </span>
                 <span class="card-preco">
                     R$ ${produto.preco}
                 </span>
-                <button>Comprar<button>
-    `;
+                <button class="btn-comprar">Comprar</button>`;
 
   return card;
 };
@@ -60,4 +59,20 @@ const carregarProdutos = (produtos) => {
   container.replaceChildren(...cards);
 };
 
+const carregarProdutos2 = (produtos) => {
+  const catalogo = document.querySelector(".card-container-catalogo");
+  const cards = produtos.map(criarCard);
+
+  catalogo.replaceChildren(...cards);
+};
+
+const carregarProdutos3 = (produtos) => {
+  const promocao = document.querySelector(".card-container-promocao");
+  const cards = produtos.map(criarCard);
+
+  promocao.replaceChildren(...cards);
+};
+
 carregarProdutos(db);
+carregarProdutos2(db);
+carregarProdutos3(db);
