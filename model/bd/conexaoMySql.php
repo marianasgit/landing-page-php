e13bdea0b279c47ee6de75a93d2a9fcf9d8d6731
MARIANA GOMES DOS SANTOS <you@example.com>
2022-03-29 16:53:43 -0300

    criando arquivos do banco de dados

---
+++ model/bd/conexaoMySql.php
@@ -0,0 +1,10 @@
+<?php
+
+/*********************************************************************
+ * Objetivo: arquivo para criar a conexão com o banco de dados MySQL
+ * Autor: Mariana
+ * Data: 29/03/2022
+ * Versão: 1.0
+ *********************************************************************/
+
+// Constantes para estabelecer a conexao com o banco de dados
